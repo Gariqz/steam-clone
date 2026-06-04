3176973dbe566bf9b39f227ad73bc2ab4f9466f7
Gariqz <ghassanariq7@gmail.com>
2026-06-04 15:16:00 +0700

    Feat added Login.fxml views page that connected to scenebuilder (#1)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,23 @@
-public class Main {
-    
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/Login.fxml"));
+        
+        Scene scene = new Scene(root, 800, 600);
+        
+        primaryStage.setTitle("Steam Clone - Login Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,3 +1,27 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
-    
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private void handleLoginBtn(ActionEvent event) {
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+        
+        System.out.println("Tombol Login Diklik!");
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+        
+        // Nanti logika nyambungin ke MySQL taruh di sini
+    }
 }
